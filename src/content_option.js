bf205704c845a4ed802ab60d040052400a3af6ee
niting <niting1209@gmail.com>
2024-05-10 15:00:48 -0400

    Updating..

--- src/content_option.js
+++ src/content_option.js
@@ -1,7 +1,7 @@
-const logotext = "NITIN";
+const logotext = "NITIN GUPTA";
 const meta = {
     title: "Nitin Gupta",
-    description: "I’m Nitin Gupta, an AI researcher, currently working in Columbia, SC",
+    description: "I’m Nitin Gupta, an AI researcher, currently pursuing a B.S. in Computer Science at the University of South Carolina",
 };
 
 const introdata = {
@@ -11,28 +11,33 @@ const introdata = {
         second: "I design intelligent systems",
         third: "I design data-driven solutions",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I am pursuing a B.S. in Computer Science, with minors in Data Science and Mathematics, at the Univeristy of South Carolina.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "Throughout my life, I have been allowed to experience many cultures and groups of people. While perspectives and languages might differ, many common challenges still limit everyone's ability to succeed. I am passionate about encountering and solving such complex issues, and pursuing a degree in Computer Science and Mathematics will help me achieve this goal by preparing me to positively impact the world. Recently participating in various kinds of research opened my eyes to the fascinating field of artificial intelligence. Ever since falling down the rabbit hole of learning about this topic, I have been enchanted with other captivating topics, such as machine learning and data analysis. I intend to further dig into these subjects throughout my career.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Machine Learning Researcher",
+        where: "Columbia, SC",
+        date: "Aug 2022 - Apr 2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Material Analysis Researcher",
+        where: "Columbia, SC",
+        date: "Jun 2022 - Nov 2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Artificial Intelligence Intern",
+        where: "Clemson, SC",
+        date: "Jun 2020 - Aug 2020",
+    },
+    {
+        jobtitle: "Volunteering Ambassador",
+        where: "Columbia, SC",
+        date: "Aug 2021 - Present",
     },
 ];
 
@@ -41,20 +46,24 @@ const skills = [{
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Java",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "C++",
         value: 80,
     },
     {
-        name: "React",
-        value: 60,
+        name: "SQL",
+        value: 80,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Pytorch",
+        value: 75,
+    },
+    {
+        name: "R",
+        value: 70,
     },
 ];
 
@@ -73,84 +82,84 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "A web app for segment-based and interactive neural style transfer.",
+        link: "https://github.com/g-nitin/stylized-segmentation",
     },
     {
         img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Coloring images with a Convolutional Autoencoders.",
+        link: "https://github.com/g-nitin/convolutional-autoencoder",
     },
     {
         img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Machine Learning & Text mining to discover Jim Crow and racially-based laws.",
+        link: "https://github.com/g-nitin/OnTheBooksUofSC",
     },
     {
         img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Interactive, web-based documents and tools for data visualizations at the USC Libraries.",
+        link: "https://github.com/g-nitin/Library-Data-Visualization",
     },
     {
         img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "A camp-system terminal-based UI.",
+        link: "https://github.com/RebeccaCun/camp-system",
     },
     {
         img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        description: "Exploring custom non-linear least squares fitting alongside comparisons to established libraries.",
+        link: "https://github.com/g-nitin/Non-Linear-Least-Sq",
     },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "niting1209@gmail.com",
+    YOUR_FONE: "(803)470-9214",
+    description: "Send me a message or contact me using the services given above.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_rh3wmsj",
+    YOUR_TEMPLATE_ID: "template_hsb5zhu",
+    YOUR_USER_ID: "BB8ndQAqJLbMs8Zic",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/g-nitin",
+    // facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/niting9",
+    // twitter: "https://twitter.com",
 };
 export {
     meta,
